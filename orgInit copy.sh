a1c7b5fb0559dd531f8c4b1f720aa4abd997e1f1
Jonathan Lupton <jlupton@salesforce.com>
2019-10-20 22:25:20 -0400

    Trying to figure out errors on branch during Shane's deployer

---
+++ orgInit copy.sh
@@ -0,0 +1,12 @@
+sfdx force:org:create -f config/project-scratch-def.json -d 7 -s -w 60
+
+sfdx force:source:push
+sfdx force:user:permset:assign -n electron
+
+sfdx force:apex:execute -f force-app/main/default/classes/FlowDemoStartUpScript.cls
+
+sfdx shane:theme:activate -n Electron
+sfdx force:org:open 
+sfdx shane:user:password:set -g User -l User -p dreamforce2019
+
+sfdx force:source:deploy -p visualization
--- orgInit.sh
+++ orgInit.sh
@@ -1,12 +1,14 @@
-sfdx force:org:create -f config/project-scratch-def.json -d 7 -s -w 60
+sfdx force:org:create -f config/project-scratch-def.json -d 30 -s -w 60
 
 sfdx force:source:push
 sfdx force:user:permset:assign -n electron
+sfdx shane:user:permset:assign -n analytics -g Integration -l User
 
-sfdx force:apex:execute -f force-app/main/default/classes/FlowDemoStartUpScript.cls
+sfdx force:source:deploy -p dataflow
+sfdx shane:analytics:dataflow:start -n Electron
 
 sfdx shane:theme:activate -n Electron
-sfdx force:org:open 
-sfdx shane:user:password:set -g User -l User -p dreamforce2019
+sfdx force:org:open
+sfdx shane:user:password:set -g User -l User -p sfdx1234
 
 sfdx force:source:deploy -p visualization
