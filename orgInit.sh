0a3ffec2fb7c043983d4e475eb12c4dc58f00fb7
Jonathan Lupton <jlupton@salesforce.com>
2019-11-06 15:25:16 -0500

    Trying to open to Sales Home instead of Set up

--- orgInit.sh
+++ orgInit.sh
@@ -6,6 +6,6 @@ sfdx force:user:permset:assign -n electron
 sfdx force:apex:execute -f force-app/main/default/classes/FlowDemoStartUpScript.cls
 
 sfdx shane:theme:activate -n Electron
-sfdx force:org:open 
+sfdx force:org:open -p lightning/page/home
 sfdx shane:user:password:set -g User -l User -p dreamforce2019
 
